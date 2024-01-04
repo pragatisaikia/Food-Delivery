bc8f035e5f82ada139e6178582146edf38e26b92
pragatisaikia <pragatisaikia2003@gmail.com>
2024-01-04 21:31:53 +0530

    updated top pick section

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Delivery = () => {
     return (
-        <div className='w-full bg-white pt-16 pb-10 px-4'>
+        <div className='w-full bg-white pt-16 pb-6 px-4'>
             <h3 className='text-orange-500 font-bold text-3xl text-center'>Quick Delivery App</h3>
             <div className=' w-full md-w-[1020px] mx-auto my-5 grid md:grid-cols-2 '>
                 <img className='w-[550px] mx-auto my-4' src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp" />
